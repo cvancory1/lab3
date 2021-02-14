7ff0563a4f59a8b773ee63846666308804a55fba
cvancory1 <cvancory1@gmail.com>
2021-02-13 19:35:43 -0500

    pushing

--- lab3.cpp
+++ lab3.cpp
@@ -15,7 +15,10 @@ Goals:
 #include <ctime>
 #include <cstdlib>
 #include<unistd.h>
+#include<cmath>
 using namespace std;
+
+
 
 struct Heap {
     int* arr; // the underlying array
@@ -36,29 +39,189 @@ struct Heap {
 
 };
 
-void MaxHeapify(){
+void MaxHeapify(Heap A, int i){
+    int left= 2*i;
+    int right = (2*i)+1;
+    int largest;
+
+    // Find the largest among node i and its children, and swap with i
+    if(left <= A.heap_size && A[left] > A[i] ){
+        largest=left;
+    }else if(right < A.heap_size && A[right] > A[largest]){
+        largest = right;
+    }else{
+        largest=i;
+    }
+
+    if(largest != i){
+        swap(A[i], A[largest]);
+        // MaxHeapify(A, largest);
+    }
+
 
 }
 
-void  BuildMaxHeap(){
+void  BuildMaxHeap(Heap A ){
+    A.heap_size= A.length; // the whole array will be a heap when we’re done
+    for( int i= A.length/2; i>=0 ; i--){
+        MaxHeapify(A,i);
+    }
+}
+
+void HeapSort(Heap A ){
+// TO DO
+// // Make sure the array is a valid heap, where the largest element must be A[1]
+//     BuildMaxHeap(A);
+//     for ( int i= A.length; i<=2 ; i)
+
 
 }
 
-void HeapSort(){
+// void printChildren(Heap A, int left , int right ){
+
+// }
+
+//utiliy functions
+
+//prints node at lable i
+void printNode(Heap A, int lable){
+    if(lable - 1 <= A.length)
+        cout << A.arr[lable - 1] << " ";
+    else {
+        cout << "* ";
+    }
+}
+
+//prints left child of node at lable i
+int printLeftChild(Heap A, int lable){
+    if((2 * lable) - 1 <= A.length)
+        cout << A.arr[(2 * lable) - 1] << " ";
+    else
+        cout << "* ";
+}
+
+//returns right child of node at lable i
+int printRightChild(Heap A, int lable){
+    if((2 * lable) <= A.length)
+        cout << A.arr[(2 * lable)] << " ";
+    else
+        cout << "* ";
+}
+
+// i specifies a node which will print looking like the root, depth specifies the depth of the "sub -tree"
+void print(Heap A, int i, int depth){
+    if(i > A.length){
+        return;
+        printNode(A,i);
+    }
+
+    
+
+    print(A, 2*i, depth);
+
+    print(A, 2*i+1, depth);
+    
+
+    //print left child
+    printLeftChild(A, i);
+
+    //print right child
+    printRightChild(A, i);
+
+    //print node
+    //printNode(A, i);
+}
+
+
+
+//    for(i; i<=A.length ;i=2*i){
+        // cout<< A.arr[(2*i)-1]<<"  "<<A.arr[(2*i)]<<endl;// left and right child
+
+   
+    //cout<<A.length<<endl;
+    //int mid = A.length;
+
+
+    // if(i > A.length){
+    //      return;// print star 
+    // }
+
+      
+
+    // if(depth>0){
+    //     cout<<A.arr[i-1]<<endl; //root
+    // }
+    
+    // cout<< A.arr[(2*i)-1]<<"  "<<A.arr[(2*i)]<<endl;// left and right child
+    // cout<<endl;
+
+    //print(A,2*i,depth);
+    // print(A,2*i,depth--);// print left child
+    // print(A,(2*i)+1,depth--); //print right child
+
+      
+
+
+//    if(depth==0){
+//        return;
+
+
+//    }else if(depth>1){
+//         for(int tab = depth-1; tab >0 ;tab --){
+//             cout<< "    ";
+//         }
+//         cout<<A.arr[i-1]<<endl;
+
+//         for(int tab = depth-1; tab >0 ;tab --){
+//             cout<< "  ";
+//         }
+//         cout<<"/   \\"<<endl;
+
+//         cout<< A.arr[(2*i)-1]<<"      "<<A.arr[(2*i)]<<endl;
+
+//         print(A,i--,depth--);
+//    }
+   
+
+void swap(int & num1, int& num2){
+
+    int temp1=num1;
+    int temp2=num2;
+    num1= temp2;
+    num2=temp1;
+
+    
 
 }
 
 
-// i specifies a node, depth specifies the depth of the "tree"
-void print(int * arr, int i, int depth){ 
 
-}
 
 
 int main(){
-    int arr= new int [10];
-    //hi 
-    int a;
+    int temp;
+    Heap heap1= { {new int[10]{ 16, 14, 10, 8, 7,9,3,2,4,1 } }, 10, 4};
+    // heap1.arr= new int [heap1.length];
+    // heap1.length=10;
+    // heap1.heap_size=10;
+    cout<<"heap1="<<heap1.length<<endl;
+
+
+    // cout<<"hi" <<endl;
+
+    for(int i=1;i<10;i++){
+        //  cin>>temp;
+        //  heap1.arr[i]=temp;
+    
+
+        cout<<heap1.arr[i]<<endl;
+    }
+
+    puts("");
+    
+    print(heap1, 2, 3);
+    // cout<<"bye"<<endl;
+    
 }
 
 
--- makefile
+++ makefile
@@ -2,6 +2,7 @@
 
 program: lab3.o
 	g++ -g -std=c++11 lab3.o -o main
+	./main
 
 lab3.o: lab3.cpp
 	g++ -g -std=c++11 -c lab3.cpp
