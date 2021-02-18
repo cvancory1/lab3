ace7bb72cf7e6067bdd0f4fd885ab1c990a0b2b8
cvancory1 <cvancory1@gmail.com>
2021-02-18 09:04:35 -0500

    hi

--- lab3.cpp
+++ lab3.cpp
@@ -28,6 +28,47 @@ struct Heap {
     int heap_size; // will change based on which portion of arr is a valid heap
 
 
+
+void makeArrRandom( ){
+    arr= new int [length];
+    for(int i=0;i<length;i++){
+        arr[i]= rand() % 1000+ 2; 
+    }
+     
+}
+
+void makeArrSorted( ){
+    arr= new int [length];
+
+     for(int i=0;i<length;i++){
+         arr[i]=i;
+     }
+
+}
+
+void makeArrBackward( ){
+    arr= new int [length];
+    int backwards=length;
+     for(int i=0;i<length;i++){
+         arr[i]= backwards;
+         backwards--;
+     }
+}
+
+// creates an array where half of the numbers are 30 and the other half are random
+void makeArrDuplicates(){
+    arr= new int [length];
+    for(int i=0;i<length;i++){
+        if(i%2 == 0){
+            arr[i]=30;
+        }else{
+             arr[i]= rand() % 1000+ 1;
+        }
+        
+    }
+}
+
+
   // give the struct an [] operator to pass through access to arr
   // we return an int reference so we can assign into the structure,
   // otherwise it would only return a copy of the indexed element
@@ -99,13 +140,7 @@ void printHeap(Heap A, int i, int depth){
         arr[j]=-9877;
     }
     print(A, i , depth , arr); // calls the above function to fill the array to be printed
-
-
-    // for (int i=0; i< A.length ;i++){
-    //     cout<<  arr[i]<<endl;;
-    // }
-    //cout<<"HERE ="<<endl;
-
+    
     int numNodes =0;
     int nodesPrinted=0;
     // prints the root
@@ -161,12 +196,6 @@ void MaxHeapify(Heap A, int i){
     int right = (2*i)+1;
     int largest;
 
-    cout<<"\nEnter Maxheapify\n"<<endl;
-
-    //printHeap(A, i, 2);
-
-    // cout<<"left= "<<left<<" right= "<<right<< " i="<<i<<endl;
-     cout<<"arrays left="<<A.arr[left-1] <<" right="<<A.arr[right-1]<<" arr[i]="<< A.arr[i-1]<<endl;
 
     // Find the largest among node i and its children, and swap with i
     if(left <= A.heap_size && A.arr[left-1] > A.arr[i-1]){
@@ -192,7 +221,7 @@ void MaxHeapify(Heap A, int i){
 
     if(largest != i ){
        swap(A.arr[i-1], A.arr[largest-1]);
-        cout<<"\n"<<endl;
+        // cout<<"\n"<<endl;
         //printHeap(A, i, 2);
 
         MaxHeapify(A, largest);
@@ -206,16 +235,16 @@ void MaxHeapify(Heap A, int i){
 * from the elements using Θ(n)time.
 */
 void  BuildMaxHeap(Heap A ){
-    puts("Enter buildmaxheap");    
+   // puts("Enter buildmaxheap");    
 
     A.heap_size= A.length; // the whole array will be a heap when we’re done
     for( int i= A.length/2; i>0 ; i--){
-        cout<< "i"<<i<<endl;
+        //cout<< "i"<<i<<endl;
         MaxHeapify(A,i);
     }
 
-    puts("exit buildmaxheap");    
-    printHeap(A,1,4);
+    //puts("exit buildmaxheap");    
+   // printHeap(A,1,4);
 
 }
 
@@ -226,22 +255,22 @@ void  BuildMaxHeap(Heap A ){
 */
 void HeapSort(Heap A ){
 // Make sure the array is a valid heap, where the largest element must be A[1]
-    puts("Enter HEAPSORT");    
+    //puts("Enter HEAPSORT");    
     BuildMaxHeap(A);
-    puts("BuildMaxheap finish");    
+    //puts("BuildMaxheap finish");    
     //printArr(A);
-    printHeap(A,1 ,4);
+    //printHeap(A,1 ,4);
 
 
-    for ( int i= A.length; i>=1 ; i--){
+    for ( int i= A.length; i>=2 ; i--){
         // We know A[i] is the largest among A[1,...,i], so move it to the back of the heap and consider it removed from the heap
-         cout<<" num1 = "<<A.arr[0]<< " num= "<< A.arr[i-1]<<endl;
+        // cout<<" num1 = "<<A.arr[0]<< " num= "<< A.arr[i-1]<<endl;
 
         swap(A.arr[0], A.arr[i-1]);
         A.heap_size--;
         // we moved one of the smaller elements to the root so now we have to Clean up
         MaxHeapify(A,1);
-        printArr(A);
+        //printArr(A);
 
 
 
@@ -250,8 +279,22 @@ void HeapSort(Heap A ){
 
 }
 
+void timeHeapSort( Heap A){
 
-   
+ 
+  auto begin = std::chrono::system_clock::now();
+  HeapSort(A);
+
+
+  auto finish = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = finish-begin;
+  std::time_t end_time = std::chrono::system_clock::to_time_t(finish);
+  std::cout << " finished at " << std::ctime(&end_time)
+    << "elapsed time: " << elapsed_seconds.count() << " seconds\n\n";
+
+    
+}
+
 
 
 
@@ -263,32 +306,52 @@ int main(){
     // things to send to maxheapify(A, any index where you want to compare a small baby tree aka size=3)
     // buildmaxheap- uses maxheapify starting at the bottom of the array to the top
     // heap sort- moves the largest elem aka i=0 to the back then maxheapifys everything to make sure it is still a valid max heap
-     int temp=1;
-     int temp2=0;
+    
+    
+    // int length=10;
+    int length=250000;
 
-    Heap heap1= {new int[10]{ 16, 14, 10, 8, 7,9,3,2,4,1 }, 10, 10};
-    Heap heap2= {new int[8] {10, 20,25,6,12,15,4,16}, 8, 8};
+    
+    // while( length!= 101000){
+         Heap heap1,heap2,heap3,heap4;
 
-    // Heap heap2;
-    // heap1.arr=new int [10];
-    // heap1.heap_size=10;
-    // heap1.length=10;
+        cout<<"length= "<<length<<endl;
+        heap1.length= length;
+        heap1.heap_size = length;
+        heap2.length= length;
+        heap2.heap_size = length;
+        heap3.length= length;
+        heap3.heap_size = length;
+        heap4.length= length;
+        heap4.heap_size = length;
 
-    printArr(heap2);
-
-   // MaxHeapify(heap2, heap2.length);
-   BuildMaxHeap(heap2);
-
-
-
-
-    printArr(heap2);
-    printHeap(heap2,1,4);
+        cout<<"random "<<length<<endl;
+        heap1.makeArrRandom();
+        timeHeapSort(heap1);
+       
+        cout<<"backward "<<length<<endl;
+        heap2.makeArrBackward();
+        timeHeapSort(heap2);
 
 
-     
-    delete [] heap1.arr;
-    delete [] heap2.arr;
+        cout<<"sorted "<<length<<endl;
+        heap3.makeArrSorted();
+        timeHeapSort(heap3);
+
+
+        cout<<"duplicates "<<length<<endl;
+        heap4.makeArrDuplicates();
+        timeHeapSort(heap4);
+
+        length+=10000;
+
+        delete [] heap1.arr;
+        delete [] heap2.arr;
+        delete [] heap3.arr;
+        delete [] heap4.arr;
+
+    // }
+
 
     
 }
